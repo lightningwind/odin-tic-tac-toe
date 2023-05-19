4e1504a5d02ed4666463d32628a49eb8daa33c7d
David Islam <david.islam24@gmail.com>
2023-05-18 21:58:07 -0700

    Create game module and Player factory

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const startGameBtn = document.querySelector('#start-btn');
 const gameboardEle = document.querySelector('#gameboard');
+const playerOneInput = document.querySelector('#player1');
+const playerTwoInput = document.querySelector('#player2');
+
 
 const gameboard = (() => {
   const board = ['', '', '', '', '', '', '', '', ''];
@@ -10,13 +13,36 @@ const gameboard = (() => {
       boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
     })
     gameboardEle.innerHTML = boardHTML;
+  }
 
-    return {
-      render,
-    }
+  return {
+    render,
   }
 })();
 
+const Player = (playerName, marker) => {
+  return { playerName, marker };
+}
+
+const game = (() => {
+  let players;
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    const player1 = Player(playerOneInput.value, 'X');
+    const player2 = Player(playerTwoInput.value, "O");
+    
+    players = [player1, player2];
+    currentPlayerIndex = 0;
+    gameOver = false;
+
+    gameboard.render();
+  }
+
+  return { start };
+})();
+
 startGameBtn.addEventListener('click', () => {
-  // TODO: Start the game
+  game.start(); 
 })
