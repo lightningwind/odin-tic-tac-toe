24e9e2306683e2aad3849cda8bdcf41df7229f28
David Islam <david.islam24@gmail.com>
2023-05-22 10:50:12 -0700

    Improve game functionality

--- app.js
+++ app.js
@@ -4,34 +4,51 @@ const playerOneInput = document.querySelector('#player1');
 const playerTwoInput = document.querySelector('#player2');
 
 
+// A gameboard module responsible for drawing the gameboard
 const gameboard = (() => {
   const board = ['', '', '', '', '', '', '', '', ''];
 
   const render = () => {
+    // Step 1: Create the squares in HTML
     let boardHTML = '';
     board.forEach((square, index) => {
       boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
     })
     gameboardEle.innerHTML = boardHTML;
+
+    // Step 2: Add click event listeners to each square
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+      square.addEventListener('click', game.handleClick);
+    })
   }
 
-  return {
-    render,
+  const update = (index, value) => {
+    board[index] = value;
+    render();
   }
+
+  const getBoard = () => {
+    return board;
+  }
+
+  return { render, update, getBoard };
 })();
 
+// A Player factory
 const Player = (playerName, marker) => {
   return { playerName, marker };
 }
 
+// A game module responsible for all the logic
 const game = (() => {
   let players;
-  let currentPlayerIndex;
+  let currentPlayerIndex; // value is either a 0 or a 1
   let gameOver;
 
   const start = () => {
     const player1 = Player(playerOneInput.value, 'X');
-    const player2 = Player(playerTwoInput.value, "O");
+    const player2 = Player(playerTwoInput.value, 'O');
     
     players = [player1, player2];
     currentPlayerIndex = 0;
@@ -40,7 +57,19 @@ const game = (() => {
     gameboard.render();
   }
 
-  return { start };
+  const handleClick = (e) => {
+    // Extract the index of this square using string manipulation
+    const index = parseInt(e.target.id.split('-')[1]);
+
+    if (gameboard.getBoard()[index] !== '') {
+      return; // Spot's taken
+    }
+
+    gameboard.update(index, players[currentPlayerIndex].marker);
+    currentPlayerIndex = Number(!currentPlayerIndex);
+  }
+
+  return { start, handleClick };
 })();
 
 startGameBtn.addEventListener('click', () => {
